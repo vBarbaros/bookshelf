b62d4624c47a71b1081eb1e38980344880c3bea6
Victor Barbaros <barbarosvictor@yahoo.com>
2018-08-03 21:39:02 -0400

    first commit

---
+++ src/main/java/com/vbarbar/bookshelf/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.vbarbar.bookshelf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+  
+	@RequestMapping("/")
+	public String welcome(Model model) {
+		model.addAttribute("greeting", "Welcome to Bookshelf!");
+		model.addAttribute("tagline", "The Greatest Place To Find The Greatest Books!");
+		return "welcome";
+	}
+}
